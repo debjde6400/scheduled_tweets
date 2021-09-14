4d3b4efcee0256dbccd2fd90749c1ac90d0d00b9
debjde6400 <debanjandey64@gmail.com>
2021-09-15 00:24:03 +0530

    Added app related files

---
+++ app/jobs/tweet_job.rb
@@ -0,0 +1,24 @@
+class TweetJob < ApplicationJob
+  queue_as :default
+
+  def perform(tweet)
+    binding.irb
+    byebug
+    return if tweet.published?
+
+    # Rescheduled a tweet to the future
+    return if tweet.publish_at > Time.current
+
+    tweet.publish_to_twitter!
+  end
+end
+
+# push publish_at forward
+# noon -> 8AM
+# 8AM -> sets tweet id
+# Noon -> published, does nothing
+
+# push publish_at future
+# 9AM -> 1PM
+# 9AM -> should do nothing
+# 1PM -> should publish tweet and set tweet_id
---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+# email:string
+# password_digest:string
+#
+# password:string virtual
+# password_confirmation:string virtual
+
+class User < ApplicationRecord
+    has_many :twitter_accounts
+    has_many :tweets
+    has_secure_password 
+
+    #this validates there is an email address prior to creating the new user.
+    validates :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
+end
